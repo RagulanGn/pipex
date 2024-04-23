8220538879fdcd6b4bad23d0476c750765f51c81
RagulanGn <113636027+RagulanGn@users.noreply.github.com>
2024-04-23 16:09:18 +0200

    Add files via upload
    main

---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgnanaso <rgnanaso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 13:31:57 by rgnanaso          #+#    #+#             */
+/*   Updated: 2024/04/23 16:01:01 by rgnanaso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	t_pipex	*pipex;
+
+	if (argc < 5)
+		return (0);
+	pipex = init_pipex(argc, argv);
+	if (parsing(pipex, argc, argv, envp) == NULL)
+		return (0);
+	execute(pipex, argv, envp, argc);
+}
